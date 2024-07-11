866ca0726e9cee2374ea993eb7c6b17769ddc4ea
Akash n <akashnagesh10@gmail.com>
2024-07-11 09:38:41 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Chat-Bot-clone/",
+  base:"/Chat-Bot-Clone/",
   plugins: [react()],
 })
